49aaf08f7a1d5deb1832345423564c521fadf33a
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-17 20:16:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+// Test Control Flow
+
+// Strings
+let myPersonality = "friendly";
+
+if (myPersonality === "mean") {
+  console.log(myPersonality);
+  
+  } else if (myPersonality === "weird") {
+    console.log(myPersonality);
+  } else {
+    console.log("Sorry none of the conditions passed!");
+}
