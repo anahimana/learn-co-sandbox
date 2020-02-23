77ebecf33329a9e2ebf753b7119e5200d6f656e2
Alex Nahimana <alex.nahimana1@gmail.com>
2020-02-23 18:15:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
+/*
 // Test Control Flow
-
-// Strings
 let myPersonality = "friendly";
 
 if (myPersonality === "mean") {
@@ -11,3 +10,43 @@ if (myPersonality === "mean") {
   } else {
     console.log("Sorry none of the conditions passed!");
 }
+
+
+
+
+// Variables
+
+// Declare with 'var'
+var a = "letter 1", b = "letter 2", c = "letter 3", d = "Hello!", e = "you found me", f = "f is for funk!";
+console.log(a);
+console.log(a, b, c, d, e, f);
+
+*/
+
+// Scope
+
+function outerFunction() {
+  var innerVariable = "I'm sort of a secret.";
+ 
+  return function innerScope() {
+    var inaccessible = "Nothing can touch me.";
+ 
+    return innerVariable;
+  };
+}
+
+// var myScope = outerFunction();
+// console.log(myScope);
+// console.log(innerScope());
+
+
+const oneTwoThree = [1, 2, 3];
+const sevenEightNine = [7, 8, 9];
+ 
+var myStuff = [4, 5, 6, ...oneTwoThree, ...sevenEightNine];
+
+// console.log(myStuff);
+
+const letters = ["alpha", "gamma", "delta"];
+console.log([...letters.slice(0, 1), "beta", ...letters.slice(1)]);
+
